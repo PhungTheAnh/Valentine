d56c305525fac979e378b00bc5fa944c94f3bf41
theanh <phungtheanh2001@gmail.com>
2025-02-13 21:07:52 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -117,7 +117,7 @@ const Footer = ({ onMusic, openMusic, setOnMusic }) => {
   return (
     <div>
       {
-        openMusic && <div onClick={handleMusic} className="music-control" id="musicControl" title="Toggle music">
+        openMusic && <div onClick={handleMusic} className="music-control">
           <svg className="music-icon" viewBox="0 0 24 24" id="musicIcon">
             {
               onMusic ? <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"></path> : <path d="M4.27 3L3 4.27l9 9v.28c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4v-1.73L19.73 21 21 19.73 4.27 3zM14 7h4V3h-6v5.18l2 2z"></path>
